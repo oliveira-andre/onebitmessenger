a0dde40ab720245c42fb2789f379a0393006a09d
Andre Oliveira <andreoliveirar2d2@gmail.com>
2019-12-09 11:10:10 -0400

    (new feature) configuring specific table

--- db/migrate/20191209144056_create_contacts.rb
+++ db/migrate/20191209144056_create_contacts.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :record, null: false, foreign_key: true
+      t.references :record, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
