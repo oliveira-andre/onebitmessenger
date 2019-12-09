c24e090bd69f6ca4cd7ee5148fa41dbfe8489eb1
Andre Oliveira <andreoliveirar2d2@gmail.com>
2019-12-09 11:10:27 -0400

    (new feature) creating two users, contact and message

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
-User.first_or_create!(name: 'onebitcode', email: 'onebitcode@gmail.com',
-                      password: '123456', password_confirmation: '123456')
+if User.all.length < 2
+  first_user = User.create!(name: 'onebitcode', email: 'onebitcode@gmail.com',
+                            password: '123456', password_confirmation: '123456')
+  secound_user = User.create(name: 'root', email: 'root@root.com',
+                             password: '123456', password_confirmation: '123456')
+  Contact.first_or_create!(user: first_user, record: secound_user)
+  Message.first_or_create!(sender: first_user, receiver: secound_user,
+                           content: 'SEEDS')
+end
