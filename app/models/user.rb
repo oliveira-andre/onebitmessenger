bc54e981def5fc4cdf94059c23779c370cac2628
Andre Oliveira <andreoliveirar2d2@gmail.com>
2019-12-09 11:09:49 -0400

    (new feature) configuring the has many from contacts, sents and receivereds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :contacts
+  has_many :sents, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :receivereds, class_name: 'Message', foreign_key: 'receiver_id'
 end
